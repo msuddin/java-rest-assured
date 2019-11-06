f34fd541ed8baf1e276a52d06ced98651c06f988
Mohammed S Uddin <mo@thetestroom.com>
2019-11-06 11:30:50 +0000

    Updated tests

--- src/test/java/com/thetestroom/PersonControllerTest.java
+++ src/test/java/com/thetestroom/PersonControllerTest.java
@@ -11,9 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringRunner.class)
@@ -30,14 +30,14 @@ public class PersonControllerTest {
 
     @Test
     public void shouldGetStatusForHi() {
-        given().when().get("/hi/billy")
+        get("/hi/billy")
                 .then()
                 .statusCode(200);
     }
 
     @Test
     public void shouldGetContentForHi() {
-        String response = given().when().get("hi/jimmy")
+        String response = get("hi/jimmy")
                 .then()
                 .extract()
                 .asString();
@@ -46,29 +46,30 @@ public class PersonControllerTest {
 
     @Test
     public void shouldGetStatusForPerson() {
-        given().when()
-                .contentType("application/json")
+        given().contentType("application/json")
                 .body(new Person("Robin", 25))
-                .when().post("/person")
+                .when()
+                .post("/person")
                 .then()
                 .statusCode(200);
     }
 
     @Test
     public void shouldGetCorrectBodyResponseFromPersonAndAssertAsString() {
-        String response = given().when()
+        String response = given()
                 .contentType("application/json")
                 .body(new Person("James Gorden", 35))
                 .post("/person")
                 .asString();
-        assertThat(response, is("{\"name\":\"James Gorden\",\"age\":35}"));
+        assertThat(response, containsString("\"name\":\"James Gorden\""));
+        assertThat(response, containsString("\"age\":35"));
     }
 
     @Test
     public void shouldGetCorrectBodyResponseFromPersonAndAssertAsJsonParts() {
         given().contentType("application/json")
-                .when()
                 .body(new Person("Gothem", 1000))
+                .when()
                 .request("POST", "/person")
                 .then()
                 .assertThat()
@@ -79,8 +80,8 @@ public class PersonControllerTest {
     @Test
     public void shouldHaveResponseTimeLessThenExpectedResponseTime() {
         Response response = given().contentType("application/json")
-                .when()
                 .body(new Person("Password", 1234))
+                .when()
                 .request("POST", "/person");
         assertThat(response.time(), is(lessThan(1500l)));
     }
@@ -88,8 +89,8 @@ public class PersonControllerTest {
     @Test
     public void shouldHaveResponseTimeLessThenFiveSeconds() {
         given().contentType("application/json")
-                .when()
                 .body(new Person("Password", 1234))
+                .when()
                 .request("POST", "/person")
                 .then()
                 .time(lessThan(5000l))
